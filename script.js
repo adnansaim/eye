4cd830b367ca436a745cf26de97aa5626c6fb75c
adnan saim <adnansaim15@yahoo.com>
2023-01-28 15:46:43 +0600

    initial eye project

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const eyeball = document.querySelector('.eyeball')
+
+
+document.addEventListener('mousemove', function (e) {
+    const x = e.clientX * 100 / window.innerWidth + '%'
+    const y = e.clientY * 100 / window.innerHeight + '%'
+
+    eyeball.style.left = x
+    eyeball.style.top = y
+    eyeball.style.transform = "translate(-" + x + ",-" + y + ")"
+})
